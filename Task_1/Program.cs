9afa2b576c04f069f43a9ff89e74f20d21daf493
Aleksandr <sosnin93@yandex.ru>
2023-08-28 21:47:08 +0300

    Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число A:");
+        double A = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите степень B:");
+        int B = int.Parse(Console.ReadLine());
+
+        double result = CalculatePower(A, B);
+
+        Console.WriteLine($"{A} в степени {B} равно {result}");
+    }
+
+    static double CalculatePower(double baseValue, int exponent)
+    {
+        double result = 1;
+
+        for (int i = 1; i <= exponent; i++)
+        {
+            result *= baseValue;
+        }
+
+        return result;
+    }
+}
+
